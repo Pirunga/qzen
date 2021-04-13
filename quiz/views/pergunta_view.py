abecd423ca169ca83960c94d4e96a76e57e02a2c
Pedro H <pedrohenriquegs2001@gmail.com>
2021-04-13 12:11:12 -0300

    feat: add pergunta and pergunta aleatoria GET routes

--- quiz/views/pergunta_view.py
+++ quiz/views/pergunta_view.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, current_app
+from http import HTTPStatus
+import random
 
 from quiz.models.pergunta_model import PerguntaModel
 from quiz.models.alternativa_model import AlternativaModel
@@ -12,17 +14,62 @@ bp_pergunta = Blueprint("pergunta_view", __name__, url_prefix="/pergunta")
 
 @bp_pergunta.route("/", methods=["GET"])
 def todas_pergutnas():
-    ...
+    try:
+        perguntas = PerguntaModel.query.all()
+
+        response = [{"id": pergunta.id, "resposta": pergunta.resposta, "temas": [tema.nome for tema in pergunta.tema_list]} for pergunta in perguntas]
+
+        return {"data": response}, HTTPStatus.OK
+
+    except:
+
+        return {"msg": "Something went wrong."}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @bp_pergunta.route("/aleatoria", methods=["GET"])
 def pergunta_aleatoria():
-    ...
+    try:
+        given_tema = request.args.get("tema")
+
+        if given_tema:
+
+            session = current_app.db.session
+            found_tema = TemaModel.query.filter(TemaModel.nome == given_tema).first()
+
+            if not found_tema:
+                return {"msg": "This 'tema' doesn't exist."}, HTTPStatus.BAD_REQUEST
+
+            pergunta = random.choice(session.query(PerguntaModel).join(PerguntaTemaModel).filter(PerguntaTemaModel.tema_id == found_tema.id).all())
+            response = {"id": pergunta.id, "resposta": pergunta.resposta, "temas": [tema.nome for tema in pergunta.tema_list]}
+
+            return {"data": response}, HTTPStatus.OK
+
+        pergunta = random.choice(PerguntaModel.query.all())
+        response = {"id": pergunta.id, "resposta": pergunta.resposta, "temas": [tema.nome for tema in pergunta.tema_list]}
+
+        return {"data": response}, HTTPStatus.OK
+
+    except:
+
+       return {"msg": "Something went wrong."}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @bp_pergunta.route("/<int:pergunta_id>", methods=["GET"])
 def pergunta_por_id(pergunta_id):
-    ...
+    try:
+        pergunta = PerguntaModel.query.get(pergunta_id)
+
+        response = {"id": pergunta.id, "resposta": pergunta.resposta, "temas": pergunta.tema_list}
+
+        return {"data": response}, HTTPStatus.OK
+
+    except AttributeError:
+
+        return {"msg": "This 'pergunta' doesn't exist."}, HTTPStatus.BAD_REQUEST
+
+    except Exception:
+
+        return {"msg": "Something went wrong."}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @bp_pergunta.route("/", methods=["POST"])
