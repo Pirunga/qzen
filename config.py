32b312feb590ee7a309499c5a65432b33a3e16ef
Luiz Almeida <6538015-luizmatheusga@users.noreply.gitlab.com>
2021-04-16 19:56:00 +0000

    Development into master

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ from os import getenv
 
 
 class Config:
+    DEBUG = False
+    TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = getenv("SECRET_KEY")
 
@@ -9,14 +11,18 @@ class Config:
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = getenv("DB_URI_ENV")
     JSON_SORT_KEYS = getenv("JSON_SORT_KEYS")
+    DEBUG = True
 
 
 class ProductionConfig(Config):
-    ...
+    FLASK_ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = getenv("DB_URI_ENV")
+    JWT_SECRET_KEY = getenv("SECRET_KEY")
 
 
 class TestConfig(Config):
-    ...
+    SQLALCHEMY_DATABASE_URI = ""
+    TESTING = True
 
 
 config_selector = {
