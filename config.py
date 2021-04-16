a51364906bcc899c3ed79f2baad5eff406c4668c
Pedro H <pedrohenriquegs2001@gmail.com>
2021-04-16 17:35:31 -0300

    fix: heroku configs

--- config.py
+++ config.py
@@ -1,22 +1,33 @@
 from os import getenv
 
 
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
 class Config:
+    DEBUG = False
+    TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = getenv("SECRET_KEY")
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = getenv("DB_URI_ENV")
+    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
     JSON_SORT_KEYS = getenv("JSON_SORT_KEYS")
+    DEBUG = True
 
 
 class ProductionConfig(Config):
-    ...
+    FLASK_ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = uri
+    JWT_SECRET_KEY = getenv("SECRET_KEY")
 
 
 class TestConfig(Config):
-    ...
+    SQLALCHEMY_DATABASE_URI = ""
+    TESTING = True
 
 
 config_selector = {
